2fd70baf41490b816725b6fccbc89a14f8a0423c
OnyxS <64073279+OnyxS@users.noreply.github.com>
2021-06-02 00:08:47 +0300

    final__mob

--- js/script.js
+++ js/script.js
@@ -21,3 +21,36 @@ const commentSlider=new Swiper('.comment__slider',{
 	 },
 });
 
+var menu = document.querySelector(".menu")
+var ham = document.querySelector(".ham")
+var xIcon = document.querySelector(".x-icon")
+var menuIcon = document.querySelector(".menu-icon")
+
+ham.addEventListener("click", toggleMenu)
+
+function toggleMenu() {
+  if (menu.classList.contains("show-menu")) {
+    menu.classList.remove("show-menu");
+    xIcon.style.display = "none";
+    menuIcon.style.display = "block";
+    body.style.overflow="auto";
+  } else {
+    menu.classList.add("show-menu");
+    xIcon.style.display = "block";
+    menuIcon.style.display = "none";
+    body.style.overflow="hidden";
+  }
+}
+
+if (window.matchMedia("(min-width:1200px)").matches) {
+ 
+} 
+else{
+  var menuLinks = document.querySelectorAll(".menu-link");
+  menuLinks.forEach(
+    function (menuLink) {
+      menuLink.addEventListener("click", toggleMenu)
+    }
+  )
+}
+
